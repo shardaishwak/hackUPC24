ce9152e461028983342263707b1a7bec165fce2d
Ishwak Sharda <ishwak.sharda@gmail.com>
2024-05-04 14:55:04 +0200

    feature: intergrate auth0

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import "@/styles/globals.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<RecoilRoot>
-			<Component {...pageProps} />
-		</RecoilRoot>
+		<Auth0Provider
+			domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+			clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+			authorizationParams={{
+				redirect_uri: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI,
+			}}
+		>
+			<RecoilRoot>
+				<Component {...pageProps} />
+			</RecoilRoot>
+		</Auth0Provider>
 	);
 }
