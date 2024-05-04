9fb351c62113da130de29cbb328b293f93f92849
Ishwak Sharda <ishwak.sharda@gmail.com>
2024-05-04 16:12:30 +0200

    GG

---
+++ client/src/recoil/index.ts
@@ -0,0 +1,76 @@
+// hash map for storing versions
+
+import { atom } from "recoil";
+
+export interface Document {
+	_id: string;
+	title: string;
+	versions: Version[];
+}
+
+export interface Version {
+	_id: number;
+	title: string;
+	content: string;
+	prompt: string;
+	created_at: Date;
+	level: number;
+}
+
+export interface User {
+	_id: string;
+	uid: string;
+	documents: Document;
+}
+interface DocumentState {
+	[key: string]: {
+		_id: string;
+		title: string;
+		versions: Version[];
+	};
+}
+
+export const documentState = atom<DocumentState>({
+	key: "documentState",
+	// hashmap of documents
+	default: {},
+});
+
+interface VersionState {
+	[key: string]: {
+		_id: number;
+		title: string;
+		content: string;
+		prompt: string;
+		created_at: Date;
+		level: number;
+	};
+}
+
+export const versionState = atom<VersionState>({
+	key: "versionState",
+	// hashmap of versions
+	default: {},
+});
+
+interface UserState {
+	_id: string;
+	uid: string;
+	documents: string[];
+}
+
+export const userState = atom<UserState>({
+	key: "userState",
+	default: {
+		_id: "",
+		uid: "",
+		documents: [],
+	},
+});
+
+export const generalState = atom({
+	key: "generalState",
+	default: {
+		locale: "en",
+	},
+});
